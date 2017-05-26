3a287ef1aca10b1af435c6749f2ca1cc74abdd6f
ackuan <ashleyc.kuan@gmail.com>
2017-05-26 15:47:18 -0400

    weather

--- explore_pur_data.R
+++ explore_pur_data.R
@@ -25,11 +25,18 @@ library(reshape2)
 ##### READ IN DATA #####
 #read in CA PUR text files of Rights-of-Way pesticide applications
 for (i in 2006:2015){
-  assign(paste('RoW_', i, sep=''), read.table(file = paste(i, '_RightsofWay.txt', sep=''), sep = "\t", header = T, fill = T, strip.white = T, na.strings = "N/A"))
+  assign(paste('RoW_', i, sep=''), read.table(file = paste(i, '_RightsofWay.txt', sep=''), sep = "\t", header = T, fill = T, strip.white = T, na.strings = "N/A", stringsAsFactors = T))
 }
+# CA public roads by county
+for (i in 2005:2014){
+  assign(paste('caRoads_', i, sep=''), read.csv(file = paste(i, '_caPublicRoads.csv', sep=''),strip.white = T,fill = T,stringsAsFactors = T))
+}
+# CA population 2004-2014
+caPop <- read.csv(file = "California_Population_1990_2014.csv", strip.white = T, fill = T)
 
 ##### COMPILE DATA ####
 df <- rbind(RoW_2006,RoW_2007,RoW_2008,RoW_2009,RoW_2010,RoW_2011,RoW_2012,RoW_2013,RoW_2014,RoW_2015)
+rdf <- rbind(caRoads_2005,caRoads_2006,caRoads_2007,caRoads_2008,caRoads_2009,caRoads_2010,caRoads_2011,caRoads_2012,caRoads_2013,caRoads_2014)
 
 df_MissingChemNames<- df %>%
   filter(is.na(CHEMICAL_NAME)) %>%
@@ -62,45 +69,43 @@ df_na <- df_calc %>%
 df_filled <- df_calc %>%
   filter(!is.na(chem_calc))
 
+rdf_total <- rdf %>%
+  select(YEAR, 'COUNTY_NAME'=COUNTY, 'COUNTY_TOTAL'=TOTAL)
+rdf_total$YEAR <- as.factor(rdf$YEAR)
+
 ##### ANALYZE DATA #####
 # filter data by unique chemicals, sum total use per chemical, and subset top 20 chemicals by year and county
 TotalAppliedPerChemical <- df_filled %>%
   select(YEAR, COUNTY_NAME, PRODUCT_NAME, CHEMICAL_NAME, chem_calc) %>%
-  group_by(YEAR, CHEMICAL_NAME) %>%
-  summarise(lbs_per_year = sum(chem_calc)) %>%
-  group_by(YEAR, lbs_per_year) %>%
-  mutate(total_lbs_per_mile = if(YEAR=="2006") (sum(lbs_per_year)/170296.67) 
-         else if(YEAR=="2007") (sum(lbs_per_year)/171154.13) 
-         else if(YEAR=="2008") (sum(lbs_per_year)/172511.33) 
-         else if(YEAR=="2009") (sum(lbs_per_year)/171873.92)
-         else if(YEAR=="2010") (sum(lbs_per_year)/172138.66)
-         else if(YEAR=="2011") (sum(lbs_per_year)/172201.63)
-         else if(YEAR=="2012") (sum(lbs_per_year)/175543.79)
-         else if(YEAR=="2013") (sum(lbs_per_year)/174991.13)
-         else if(YEAR=="2014") (sum(lbs_per_year)/174802.85)
-         else if(YEAR=="2015") (sum(lbs_per_year)/174802.85))
+  group_by(YEAR, COUNTY_NAME, CHEMICAL_NAME) %>%
+  summarise(lbs_per_year = sum(chem_calc))
+
+chemByCounty <- TotalAppliedPerChemical %>%
+  full_join(rdf_total, by = c("YEAR","COUNTY_NAME")) %>%
+  group_by(YEAR, COUNTY_NAME, CHEMICAL_NAME, COUNTY_TOTAL) %>%
+  mutate(lbs_per_mile = lbs_per_year/(as.double(factor(levels(COUNTY_TOTAL))[COUNTY_TOTAL])))
 
 # subset top 10 chemicals used per county per year
-Top10Chemicals <- TotalAppliedPerChemical %>%
+Top5Chemicals <- chemByCounty %>%
   filter(!"GLYPHOSATE, ISOPROPYLAMINE SALT" %in% CHEMICAL_NAME) %>%
-  group_by(YEAR) %>%
-  top_n(10, total_lbs_per_mile)
+  group_by(YEAR, COUNTY_NAME) %>%
+  top_n(5, lbs_per_mile)
 
-bc_TotalAppliedPerChemical <- df_filled %>%
-  select(YEAR, COUNTY_NAME, PRODUCT_NAME, CHEMICAL_NAME, chem_calc) %>%
-  group_by(YEAR, CHEMICAL_NAME, COUNTY_NAME) %>%
-  summarise(lbs_per_year = sum(chem_calc)) %>%
-  group_by(YEAR, lbs_per_year, COUNTY_NAME) %>%
-  mutate(total_lbs_per_mile = if(YEAR=="2006") (sum(lbs_per_year)/170296.67)
-         else if(YEAR=="2007") (sum(lbs_per_year)/171154.13)
-         else if(YEAR=="2008") (sum(lbs_per_year)/172511.33)
-         else if(YEAR=="2009") (sum(lbs_per_year)/171873.92)
-         else if(YEAR=="2010") (sum(lbs_per_year)/172138.66)
-         else if(YEAR=="2011") (sum(lbs_per_year)/172201.63)
-         else if(YEAR=="2012") (sum(lbs_per_year)/175543.79)
-         else if(YEAR=="2013") (sum(lbs_per_year)/174991.13)
-         else if(YEAR=="2014") (sum(lbs_per_year)/174802.85)
-         else if(YEAR=="2015") (sum(lbs_per_year)/174802.85))
+# bc_TotalAppliedPerChemical <- df_filled %>%
+#   select(YEAR, COUNTY_NAME, PRODUCT_NAME, CHEMICAL_NAME, chem_calc) %>%
+#   group_by(YEAR, CHEMICAL_NAME, COUNTY_NAME) %>%
+#   summarise(lbs_per_year = sum(chem_calc)) %>%
+#   group_by(YEAR, lbs_per_year, COUNTY_NAME) %>%
+#   mutate(total_lbs_per_mile = if(YEAR=="2006") (sum(lbs_per_year)/170296.67)
+#          else if(YEAR=="2007") (sum(lbs_per_year)/171154.13)
+#          else if(YEAR=="2008") (sum(lbs_per_year)/172511.33)
+#          else if(YEAR=="2009") (sum(lbs_per_year)/171873.92)
+#          else if(YEAR=="2010") (sum(lbs_per_year)/172138.66)
+#          else if(YEAR=="2011") (sum(lbs_per_year)/172201.63)
+#          else if(YEAR=="2012") (sum(lbs_per_year)/175543.79)
+#          else if(YEAR=="2013") (sum(lbs_per_year)/174991.13)
+#          else if(YEAR=="2014") (sum(lbs_per_year)/174802.85)
+#          else if(YEAR=="2015") (sum(lbs_per_year)/174802.85))
 
 
 
@@ -113,13 +118,15 @@ cbPalette_top10 <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#00
                      "light purple", "black", "dark orange", "light red", "dark red", "red", "brown",
                      "light brown", "light gray", "indigo")
 
-ggplot(Top10Chemicals, aes(x=YEAR, y = total_lbs_per_mile, fill=CHEMICAL_NAME)) +
+ggplot(Top5Chemicals, aes(x=YEAR, y = lbs_per_mile, fill=CHEMICAL_NAME)) +
   geom_bar(stat = "identity", position = "dodge", width = 0.5) +
-  coord_flip() +
+  #coord_flip() +
   #geom_line(aes(group=factor(CHEMICAL_NAME)), size=1) +
-  #scale_y_log10() +
+  scale_y_log10() +
   #scale_color_discrete() +
   scale_color_manual(values = cbPalette_top10) +
-  ylab("Pounds of A.I. (per mile)") 
+  facet_wrap(~COUNTY_NAME)+
+  ylab("Pounds of A.I. (per mile)") +
+  theme(legend.position = "none")
 
 
